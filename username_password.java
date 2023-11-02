aa8e7a1dd9bbf449142c7fce45a90aedc754d5b8
Samyuktha-b20 <126759970+Samyuktha-b20@users.noreply.github.com>
2023-11-02 23:45:55 +0530

    Add files via upload

---
+++ username_password.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class username_password{
+    public static void main(String[]args){
+    String[] x={"samyu","ram","sanjay"};
+    String[] y={"123","456","789"};
+      System.out.println(x[2]);
+     }
+   }
+		
