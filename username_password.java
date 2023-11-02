cfe7afba6ce96e3e01e99c69aa5498d4490f6eb8
Samyuktha-b20 <126759970+Samyuktha-b20@users.noreply.github.com>
2023-11-02 23:46:45 +0530

    Update username_password.java

--- username_password.java
+++ username_password.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class username_password{
     public static void main(String[]args){
-    String[] x={"samyu","ram","sanjay"};
+    String[] x={"samyu","ram","arun"};
     String[] y={"123","456","789"};
       System.out.println(x[2]);
      }
